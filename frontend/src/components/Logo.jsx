b17cd43dba4ac00ef80ac4ebb35020af65ffe739
Harshadbankar1596 <harshadbankar1596@gmail.com>
2025-12-29 18:06:26 +0530

    as

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,28 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Logo = ({ size = 64, color }) => {
+const Logo = ({ size = 64 }) => {
   return (
-    <div className='rounded-full'>
-      {color === "black" ? (
-        <img
-          className="logo-spin rounded-full cursor-pointer  drop-shadow-[0_0_10px_rgba(25,255,255,0.5)]"
-          src="./blacklogo.jpg"
-          alt="Logo"
-          style={{
-            width: `${size}px`,
-            height: `${size}px`,
-          }}
-        />
-      ) : <img
-        className="logo-spin rounded-full cursor-pointer  drop-shadow-[0_0_10px_rgba(25,255,255,0.5)]"
-        src="./logo.png"
-        alt="Logo"
-        style={{
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-      />}
-    </div>
+    <img
+      src="/tcc.png"
+      alt="Logo"
+      // style={{ width: size, height: size }}
+      className="cursor-pointer select-none"
+    />
   );
 };
 
